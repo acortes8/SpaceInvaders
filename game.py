902e2f37090dfec3681ba0252d59d955db622b27
Marco Cortes <16551788+mcortes95@users.noreply.github.com>
2019-03-11 20:47:07 -0500

    fds

--- game.py
+++ game.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
-
+import ship
 import pyxel
 
 class S_Invaders:
     def __init__(self):
         pyxel.init(200,250,caption="Space Invaders" ,fps=60)
         pyxel.run(self.update,self.draw)
-
+        self.p1=ship.ship()
+        
     def update(self):
+        self.p1.update()
         if pyxel.btnp(pyxel.KEY_Q):
             pyxel.quit()
 
     def draw(self):
         pyxel.cls(0)
         pyxel.rect(50,50,100,100,5)
-        
+        self.p1.draw() 
 
 
 S_Invaders()
---
+++ ship.py
@@ -0,0 +1,9 @@
+
+class ship:
+    def __init__(self):
+
+
+    def update(self):
+
+    def draw(self):
+
